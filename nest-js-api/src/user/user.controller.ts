ec2247606c7214f307a406c81fa73a010634d09f
Rishikesht1245 <rishikeshtgit@gmail.com>
2024-07-08 22:17:02 +0530

    Update : created custom decorator to access the user object from request.

--- nest-js-api/src/user/user.controller.ts
+++ nest-js-api/src/user/user.controller.ts
@@ -1,14 +1,21 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
 import { Request } from 'express';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
 
 @Controller('users')
 export class UserController {
     // jwt is the strategy we are using
-    @UseGuards(AuthGuard('jwt'))
+    // @UseGuards(AuthGuard('jwt'))
+    // instead of the above method we can create a custom guard and use it
+    @UseGuards(JwtGuard)
     @Get('me')
-    getMe(@Req() req:Request){
-        console.log({user: req.user})
-        return req.user;
+    // getMe(@Req() req:Request){
+    // instead the above we can use like the below using the custom decorator
+    getMe(@GetUser('') user : User){
+        console.log({user})
+        return user;
     }
 }
