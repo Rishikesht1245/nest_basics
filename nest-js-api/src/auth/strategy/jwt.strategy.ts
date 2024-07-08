a0aaa502d75c4f1aa1b0a659bbee01f95949be5b
Rishikesht1245 <rishikeshtgit@gmail.com>
2024-07-08 21:57:45 +0530

    Update : created the get user route.

--- nest-js-api/src/auth/strategy/jwt.strategy.ts
+++ nest-js-api/src/auth/strategy/jwt.strategy.ts
@@ -2,11 +2,13 @@ import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt'){
-    constructor(private config : ConfigService){
+    constructor(private config : ConfigService, private prisma:PrismaService){
         // calling the parent constructor
+        // super will be called before anything so this cannot be called inside
         super({
             jwtFromRequest : ExtractJwt.fromAuthHeaderAsBearerToken(),
             secretOrKey : config.get("JWT_SECRET")
@@ -15,10 +17,14 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt'){
 
     //  function to validate for the auth guard
     // token will be transformed into this object
-    validate (payload : any){
+    async validate (payload : {sub:number, email : string}){
         console.log({payload});
-        // it will be attaced as req.user object 
-        return payload;
+
+        const user = await this.prisma.user.findUnique({where: {id : payload.sub}});
+
+        delete user.hash;
+        // whatever we return here, it will be attaced as req.user object 
+        return user;
     }
 
 }
