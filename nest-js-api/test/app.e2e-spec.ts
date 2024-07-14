0e3a876500cb3ad46b8c194f057f81adc5815ca2
Rishikesht1245 <rishikeshtgit@gmail.com>
2024-07-14 16:20:34 +0530

    Update : e2e testing incompleted files.

--- nest-js-api/test/app.e2e-spec.ts
+++ nest-js-api/test/app.e2e-spec.ts
@@ -1,14 +1,32 @@
 import { Test } from "@nestjs/testing";
 import { AppModule } from "../src/app.module";
+import { INestApplication, ValidationPipe } from "@nestjs/common";
 
 describe('App e2e', () => {
+  let app : INestApplication;
   // we are using jest for testing oru application
   beforeAll(async () => {
     // creating testing module on our app
     const moduleRef = await Test.createTestingModule({
       imports:[AppModule],
     }).compile();
+
+    // creating the testing application
+     app = moduleRef.createNestApplication();
+
+     // include the pipes for validation 
+     app.useGlobalPipes(
+      new ValidationPipe({
+        // for stripping the unwanted inputs which are not mentioned in the class validators(interface)
+        whitelist: true,
+      }),
+    );
+    // start the server
+    await app.init()
   });
+
+  // closing the app after everything
+  afterAll(() => app.close())
 
   it.todo("should pass");
 
