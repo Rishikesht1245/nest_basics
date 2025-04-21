9977e0a8459936a78cc4aa02db86728dd3df6ca0
Rishikesht1245 <rishikeshtgit@gmail.com>
2025-04-21 08:58:21 +0530

    postgres sync connection

--- nest-js-advanced/1-nest-intro/src/app.module.ts
+++ nest-js-advanced/1-nest-intro/src/app.module.ts
@@ -4,10 +4,21 @@ import { AppService } from './app.service';
 import { UserModule } from './users/users.module';
 import { TweetModule } from './tweet/tweet.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   // import user defined modules here : then only nest js will know those modules are there
-  imports: [UserModule, TweetModule, AuthModule],
+  // syncronous connect using for root method
+  imports: [UserModule, TweetModule, AuthModule, TypeOrmModule.forRoot({
+    type :"postgres",
+    entities :[],
+    synchronize : true,
+    host : 'localhost',
+    port : 5433,
+    username : "postgres",
+    password : "admin@123",
+    database : "nestjs"
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
